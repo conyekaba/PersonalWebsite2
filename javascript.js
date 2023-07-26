61c8be7656044071456c48c97cc6850ee1c023a4
conyekaba <136660593+conyekaba@users.noreply.github.com>
2023-07-25 20:54:58 -0400

    changes

---
+++ javascript.js
@@ -0,0 +1,7 @@
+<script>
+    // simple javascript code to display an alert when the page is loaded
+    window.onload=function () {
+        alert('welcome to chukwuemeka onyekaba\'s website!')
+    }
+        
+</script>
